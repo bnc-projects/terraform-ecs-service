9dc1c118acc6485f1293fa89b6a026f920a6138e
Bhavik Kumar <bhavik@bravenewcoin.com>
2019-05-24 14:49:19 +1200

    Update for terraform 0.12

--- main.tf
+++ main.tf
@@ -17,83 +17,83 @@ data "aws_iam_policy_document" "service_assume_role" {
 }
 
 data "template_file" "task_definition" {
-  template = "${file("${path.module}/task-definitions/service.json")}"
+  template = file(format("%s/task-definitions/service.json", path.module))
   vars {
-    container_healthcheck    = "${var.container_healthcheck}"
-    container_name           = "${var.service_name}"
-    container_port           = "${var.container_port}"
-    cpu_reservation          = "${var.cpu_reservation}"
-    docker_image_url         = "${var.docker_image}"
-    healthcheck_grace_period = "${var.healthcheck_grace_period}"
-    java_options             = "${var.java_options}"
-    memory_limit             = "${var.memory_limit}"
-    memory_reservation       = "${var.memory_reservation}"
-    splunk_token             = "${var.splunk_token}"
-    splunk_url               = "${var.splunk_url}"
-    spring_profile           = "${var.spring_profile}"
+    container_healthcheck    = var.container_healthcheck
+    container_name           = var.service_name
+    container_port           = var.container_port
+    cpu_reservation          = var.cpu_reservation
+    docker_image_url         = var.docker_image
+    healthcheck_grace_period = var.healthcheck_grace_period
+    java_options             = var.java_options
+    memory_limit             = var.memory_limit
+    memory_reservation       = var.memory_reservation
+    splunk_token             = var.splunk_token
+    splunk_url               = var.splunk_url
+    spring_profile           = var.spring_profile
   }
 }
 
 resource "aws_lb_target_group" "target_group" {
-  deregistration_delay = "${var.deregistration_delay}"
+  deregistration_delay = var.deregistration_delay
   health_check {
-    healthy_threshold   = "${var.healthy_threshold}"
+    healthy_threshold   = var.healthy_threshold
     interval            = 10
     matcher             = "200-299"
-    path                = "${var.healthcheck_path}"
+    path                = var.healthcheck_path
     protocol            = "HTTP"
     timeout             = 5
-    unhealthy_threshold = "${var.unhealthy_threshold}"
+    unhealthy_threshold = var.unhealthy_threshold
   }
-  name                 = "${substr("tg-${var.service_name}", 0, min(length("tg-${var.service_name}"), 32))}"
+  name                 = substr(format("tg-%s", var.service_name), 0, min(length(format("tg-%s", var.service_name), 32))
   port                 = 80
   protocol             = "HTTP"
-  vpc_id               = "${var.vpc_id}"
-  tags                 = "${var.tags}"
+  vpc_id               = var.vpc_id
+  tags                 = var.tags
 }
 
 resource "aws_lb_listener_rule" "https_listener_rule" {
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
   condition {
     field  = "path-pattern"
     values = [
-      "${var.application_path}/*",
+      "${}var.application_path}/*",
     ]
   }
-  listener_arn = "${var.is_exposed_externally ? var.external_lb_listener_arn : var.internal_lb_listener_arn}"
+  listener_arn = var.is_exposed_externally ? var.external_lb_listener_arn : var.internal_lb_listener_arn
 }
 
 resource "aws_iam_role" "service" {
-  name               = "${var.service_name}"
-  assume_role_policy = "${data.aws_iam_policy_document.service_assume_role.json}"
-  tags               = "${var.tags}"
+  name               = var.service_name
+  assume_role_policy = data.aws_iam_policy_document.service_assume_role.json
+  tags               = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_service_policy" {
-  role       = "${aws_iam_role.service.name}"
-  policy_arn = "${var.service_role_arn}"
+  role       = aws_iam_role.service.name
+  policy_arn = var.service_role_arn
 }
 
 resource "aws_ecs_task_definition" "service" {
-  container_definitions = "${data.template_file.task_definition.rendered}"
-  family                = "${var.service_name}-Task"
-  task_role_arn         = "${var.task_role_arn}"
-  tags                  = "${var.tags}"
+  container_definitions = data.template_file.task_definition.rendered
+  family                = format("%s-Task", var.service_name)
+  task_role_arn         = var.task_role_arn
+  tags                  = var.tags
 }
 
 resource "aws_ecs_service" "service" {
-  name                              = "${var.service_name}"
-  cluster                           = "${var.cluster_name}"
-  desired_count                     = "${var.desired_count}"
-  health_check_grace_period_seconds = "${var.healthcheck_grace_period}"
-  iam_role                          = "${aws_iam_role.service.arn}"
+  name                              = var.service_name
+  cluster                           = var.cluster_name
+  desired_count                     = var.desired_count
+  health_check_grace_period_seconds = var.healthcheck_grace_period
+  iam_role                          = aws_iam_role.service.arn
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}"
-    container_name   = "${var.service_name}"
-    container_port   = "${var.container_port}"
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = var.service_name
+    container_port   = var.container_port
   }
   ordered_placement_strategy {
     type  = "spread"
@@ -107,7 +107,7 @@ resource "aws_ecs_service" "service" {
     type  = "binpack"
     field = "memory"
   }
-  task_definition                   = "${aws_ecs_task_definition.service.arn}"
+  task_definition                   = aws_ecs_task_definition.service.arn
   lifecycle {
     ignore_changes = [
       "desired_count"
@@ -117,42 +117,42 @@ resource "aws_ecs_service" "service" {
 
 resource "aws_cloudwatch_metric_alarm" "http_target_5xx_alarm" {
   alarm_actions       = [
-    "${var.alarm_actions}"
+    var.alarm_actions
   ]
-  alarm_description   = "${var.service_name} HTTP 500 response code alarm"
-  alarm_name          = "${var.service_name}-HTTP-5XX-Alarm"
+  alarm_description   = format("%s HTTP 500 response code alarm", var.service_name)
+  alarm_name          = format("%s-HTTP-5XX-Alarm", var.service_name)
   comparison_operator = "GreaterThanThreshold"
   dimensions {
-    LoadBalancer = "${var.is_exposed_externally ? var.external_lb_name : var.internal_lb_name}"
-    TargetGroup  = "${aws_lb_target_group.target_group.arn_suffix}"
+    LoadBalancer = var.is_exposed_externally ? var.external_lb_name : var.internal_lb_name
+    TargetGroup  = aws_lb_target_group.target_group.arn_suffix
   }
   evaluation_periods  = 1
   metric_name         = "HTTPCode_Target_5XX_Count"
   namespace           = "AWS/ApplicationELB"
   period              = "60"
   statistic           = "Sum"
-  tags                = "${var.tags}"
+  tags                = var.tags
   threshold           = 0
   treat_missing_data  = "notBreaching"
 }
 
 resource "aws_cloudwatch_metric_alarm" "service_not_healthy_alarm" {
   alarm_actions       = [
-    "${var.alarm_actions}"
+    var.alarm_actions
   ]
-  alarm_description   = "${var.service_name} service has no healthy instances"
-  alarm_name          = "${var.service_name}-not-healthy"
+  alarm_description   = format("%s service has no healthy instances", var.service_name)
+  alarm_name          = format("%s-not-healthy", var.service_name)
   comparison_operator = "LessThanThreshold"
   dimensions {
-    LoadBalancer = "${var.is_exposed_externally ? var.external_lb_name : var.internal_lb_name}"
-    TargetGroup  = "${aws_lb_target_group.target_group.arn_suffix}"
+    LoadBalancer = var.is_exposed_externally ? var.external_lb_name : var.internal_lb_name
+    TargetGroup  = aws_lb_target_group.target_group.arn_suffix
   }
   evaluation_periods  = 1
   metric_name         = "HealthyHostCount"
   namespace           = "AWS/ApplicationELB"
   period              = "60"
   statistic           = "Minimum"
-  tags                = "${var.tags}"
+  tags                = var.tags
   threshold           = 0
   treat_missing_data  = "breaching"
 }
--- variables.tf
+++ variables.tf
@@ -1,127 +1,152 @@
 variable "alarm_actions" {
   description = "The list of ARNs which will be triggered when the alarms trigger"
-  type        = "list"
+  type        = list
 }
 
 variable "application_path" {
   description = "The path that the service will listen to requests on"
+  type        = string
 }
 
 variable "cluster_name" {
   description = "The name of the cluster where the service will be launched"
+  type        = string
 }
 
 variable "container_healthcheck" {
   default     = "wget --quiet --tries=1 --spider --timeout=30 http://localhost:8080/actuator/health || exit 1"
   description = "The command to run for the container healthcheck"
+  type        = string
 }
 
 variable "container_port" {
   default     = 8080
   description = "The port number which the application is listening to inside the container"
+  type        = number
 }
 
 variable "cpu_reservation" {
   default     = 128
   description = "The amount of CPU to reserve on the cluster for the task"
+  type        = number
 }
 
 variable "desired_count" {
   default     = 2
   description = "The desired amount of services running at any given time"
+  type        = number
 }
 
 variable "deregistration_delay" {
   default     = 30
   description = "The number of seconds the load balancer waits before setting the service to unused from draining"
+  type        = number
 }
 
 variable "docker_image" {
   description = "The docker image to be used for the task definition"
+  type        = string
 }
 
 variable "external_lb_listener_arn" {
   description = "The external load balancers ARN"
+  type        = string
 }
 
 variable "external_lb_name" {
   description = "The external load balancer name"
+  type        = string
 }
 
 variable "healthcheck_grace_period" {
   default     = 300
   description = "The grace period to give the healthchecks"
+  type        = number
 }
 
 variable "healthcheck_path" {
   default     = "/actuator/health"
   description = "The healthcheck path"
+  type        = string
 }
 
 variable "healthy_threshold" {
   default     = 2
   description = "The number of healthchecks until a service is deemed healthy"
+  type        = number
 }
 
 variable "internal_lb_listener_arn" {
   description = "The internal load balancers ARN"
+  type        = string
 }
 
 variable "internal_lb_name" {
   description = "The internal load balancer name"
+  type        = string
 }
 
 variable "java_options" {
   default     = ""
   description = "The Java Options environment variables to apply in the container"
+  type        = string
 }
 
 variable "is_exposed_externally" {
   default     = false
   description = "Determines if the service will be attached to the external load balancer"
+  type        = bool
 }
 
 variable "memory_limit" {
   default     = 512
   description = "The hard memory limit for the task"
+  type        = number
 }
 
 variable "memory_reservation" {
   default     = 512
   description = "The amount of memory to reserve for the task"
+  type        = number
 }
 
 variable "priority" {
   default     = 1
   description = "The priority of the application path matching"
+  type        = number
 }
 
 variable "service_name" {
   description = "The name of the service which will be deployed"
+  type        = string
 }
 
 variable "service_role_arn" {
   default     = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
   description = "The ARN of the IAM role which will be attached at the service level"
+  type        = string
 }
 
 variable "splunk_token" {
   description = "The Splunk token for the HTTP Event Collector"
+  type        = string
 }
 
 variable "splunk_url" {
   description = "The Splunk URL for the HTTP Event Collector"
+  type        = string
 }
 
 variable "spring_profile" {
   default     = ""
   description = "The spring profile(s) which will be enabled in the application"
+  type        = string
 }
 
 variable "tags" {
   default     = {}
   description = "The map of tags to be set on resources"
-  type        = "map"
+  type        = map(string)
 }
 
 variable "task_role_arn" {
@@ -132,8 +157,10 @@ variable "task_role_arn" {
 variable "unhealthy_threshold" {
   default     = 3
   description = "The number of healthchecks until a service is deemed unhealthy"
+  type        = number
 }
 
 variable "vpc_id" {
   description = "The VPC ID which the load balancer listener(s) will be part of"
+  type        = string
 }
