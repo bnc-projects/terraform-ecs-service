9a521a70a76359377dd93a62c9db36ffb76a0339
James Little <alphafoobar@users.noreply.github.com>
2020-06-23 21:32:56 +1200

    Variables unexpected in outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,7 @@
 output "target_group_name" {
-  index = 0
-  value = var.attach_load_balancer ? aws_lb_target_group.target_group[index].name : ""
+  value = var.attach_load_balancer ? aws_lb_target_group.target_group[0].name : ""
 }
 
 output "ecs_service_name" {
-  index = 0
-  value = var.launch_type == "EC2" ? aws_ecs_service.ec2_service[index].id : aws_ecs_service.fargate_service[index].id
+  value = var.launch_type == "EC2" ? aws_ecs_service.ec2_service[0].id : aws_ecs_service.fargate_service[0].id
 }
