23e2e912950a45df0d2b6a716ce2803279897c32
James Little <alphafoobar@users.noreply.github.com>
2020-06-23 21:20:54 +1200

    Adding index = 0

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "target_group_name" {
-  value = var.attach_load_balancer ? aws_lb_target_group.target_group[count.index].name : ""
+  index = 0
+  value = var.attach_load_balancer ? aws_lb_target_group.target_group[index].name : ""
 }
 
 output "ecs_service_name" {
-  value = var.launch_type == "EC2" ? aws_ecs_service.ec2_service[count.index].id : aws_ecs_service.fargate_service[count.index].id
+  index = 0
+  value = var.launch_type == "EC2" ? aws_ecs_service.ec2_service[index].id : aws_ecs_service.fargate_service[index].id
 }
